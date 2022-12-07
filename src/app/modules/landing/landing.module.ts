52996fb69ef5ff32ab9f5165d707e4f2b31270b5
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-12-07 16:35:48 +0100

    feat: landing page done

---
+++ src/app/modules/landing/landing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroSectionComponent } from 'src/app/components/landing-page/hero-section/hero-section.component';
+import { MoreInfoComponent } from 'src/app/components/landing-page/more-info/more-info.component';
+import { SubscribeComponent } from 'src/app/components/landing-page/subscribe/subscribe.component';
+import { LandingPageComponent } from 'src/app/pages/landing-page/landing-page.component';
+import { HeaderComponent } from 'src/app/shared/header/header.component';
+import { FooterComponent } from 'src/app/shared/footer/footer.component';
+@NgModule({
+  declarations: [
+    HeroSectionComponent,
+    MoreInfoComponent,
+    SubscribeComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  exports: [
+    HeroSectionComponent,
+    MoreInfoComponent,
+    SubscribeComponent,
+    LandingPageComponent,
+  ],
+  imports: [CommonModule],
+})
+export class LandingModule {}
