78e2012ba2403ed9026a3c6c4c511c74a0b4f935
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-12-12 15:55:21 +0100

    feat: single event fetched

---
+++ src/app/components/events-page/event-detail/event-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EventsService } from 'src/app/services/events.service';
+import { Subscription } from 'rxjs';
+@Component({
+  selector: 'app-event-detail',
+  templateUrl: './event-detail.component.html',
+  styleUrls: ['./event-detail.component.css'],
+})
+export class EventDetailComponent implements OnInit, OnDestroy {
+  event: Event | null = null;
+  querySub!: Subscription;
+  constructor(
+    private eventService: EventsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  id!: string;
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+    if (this.id.trim() == '') this.router.navigate(['/events']);
+    console.log(this.id);
+    this.eventService.fetchSingle(this.id).subscribe((res: any) => {
+      this.event = res.item;
+      console.log(this.event);
+    });
+  }
+  ngOnDestroy(): void {
+    this.querySub.unsubscribe();
+  }
+}
